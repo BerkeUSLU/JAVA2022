1fbc519f798687297f1267d44400916112d16b30
Berke USLU <berkeuslu0@gmail.com>
2022-09-23 23:50:54 +0300

    -initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("(2022) Yazılım Geliştirici Yetiştirme Kampı - JAVA");
+        System.out.println("2");
     }
 }
