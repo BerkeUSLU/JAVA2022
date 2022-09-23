e0a83e7849b376dce7b1a6793ffb6785d52964ba
Berke USLU <berkeuslu0@gmail.com>
2022-09-23 23:51:34 +0300

    -updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("(2022) Yazılım Geliştirici Yetiştirme Kampı - JAVA");
-        System.out.println("2");
     }
 }
