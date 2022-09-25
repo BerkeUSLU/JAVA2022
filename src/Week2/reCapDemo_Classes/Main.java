06588b01088e5ea55ad95bf93c1e3f5a7db23fb3
Berke USLU <berkeuslu0@gmail.com>
2022-09-25 15:43:07 +0300

    -reCapDemo_Classes update

---
+++ src/Week2/reCapDemo_Classes/Main.java
@@ -0,0 +1,11 @@
+package Week2.reCapDemo_Classes;
+
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+
+        int sonuc = dortIslem.Topla(3,5);
+        System.out.println(sonuc);
+
+    }
+}
