3a30f3ba602e58768bd52f2f9643594c4e796f11
Berke USLU <berkeuslu0@gmail.com>
2022-09-25 18:54:42 +0300

    -methodOverloading

---
+++ src/Week2/methodOverloading/Main.java
@@ -0,0 +1,9 @@
+package Week2.methodOverloading;
+
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        System.out.println(dortIslem.topla(2,3));
+        System.out.println(dortIslem.topla(2,3,5));
+    }
+}
