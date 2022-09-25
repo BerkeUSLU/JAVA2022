32006f1ab720373d68b2d1344cac23117ac6cdea
Berke USLU <berkeuslu0@gmail.com>
2022-09-25 14:36:32 +0300

    -variable_arguments update

---
+++ src/Week2/variable_arguments/Main.java
@@ -0,0 +1,20 @@
+package Week2.variable_arguments;
+
+public class Main {
+    public static void main(String[] args) {
+        int toplam = topla2(2,3,4,5,6,10);
+        System.out.println(toplam);
+    }
+
+    public static int topla(int sayi1, int sayi2) {
+        return sayi1 + sayi2;
+    }
+    public static int topla2(int... sayilar) {
+        int toplam = 0;
+        for (int sayi: sayilar) {
+            toplam += sayi;
+        }
+        return toplam;
+    }
+
+}
